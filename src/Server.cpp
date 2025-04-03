bf3a4d3a0086ffa675514a4c3d514b9b0848b857
Ryan Lei <135535159+RyanL3i@users.noreply.github.com>
2025-04-02 22:15:01 -0500

    working through bind port

--- src/Server.cpp
+++ src/Server.cpp
@@ -13,7 +13,9 @@ int main(int argc, char **argv) {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
   
+  // create TCP socket, using IPv4 (AF_INET) and TCP (SOCK_STREAM)
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+  // if the socket returns a negative number, means failed
   if (server_fd < 0) {
    std::cerr << "Failed to create server socket\n";
    return 1;
@@ -22,15 +24,25 @@ int main(int argc, char **argv) {
   // Since the tester restarts your program quite often, setting SO_REUSEADDR
   // ensures that we don't run into 'Address already in use' errors
   int reuse = 1;
+  // takes in socket we created, sol_socket -> socket level and not TCP, so_reuseaddr -> allows reuse of local addresses
+  // pass in pointer address to option_value "&reuse", and passes in 4 (size of int) meaning "read 4 bytes"
+  // if 
   if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
     std::cerr << "setsockopt failed\n";
     return 1;
   }
   
+  // struct sockaddr_in has a few fields:
+    // short sin_family -> address family, AF_INET for IPv4
+    // unsigned short sin_port -> port number, must be in network byte order which is big endian
+    // struct in_addr sin_addr -> IP address in network byte order, where the struct in_addr has field s_addr, 32-bit that stores IPv4 addr
   struct sockaddr_in server_addr;
   server_addr.sin_family = AF_INET;
+  // takes IP connections from anywhere
   server_addr.sin_addr.s_addr = INADDR_ANY;
+  // htons converts to network byte order
   server_addr.sin_port = htons(6379);
+  
   
   if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
     std::cerr << "Failed to bind to port 6379\n";
@@ -51,7 +63,7 @@ int main(int argc, char **argv) {
   std::cout << "Logs from your program will appear here!\n";
 
   // Uncomment this block to pass the first stage
-  // 
+  // this is to bind to a por
   accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
   std::cout << "Client connected\n";
   close(server_fd);
